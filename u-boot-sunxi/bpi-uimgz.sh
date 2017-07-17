8b19df8c7544e0ba720385094b428dd68859a0ff
Banana Pi -BPI <lionwang@sinovoip.com.cn>
2017-07-17 17:34:46 +0800

    u-boot-sunxi: sync to https://github.com/BPI-SINOVOIP/BPI-u-boot.git

---
+++ u-boot-sunxi/bpi-uimgz.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BOARD=$1
+if [ -z $BOARD ] ; then
+	echo "usage: $0 BOARD"
+	exit 1
+fi
+
+echo ${BOARD}
+UBOOTBIN=out/${BOARD}/u-boot-sunxi-with-spl.bin
+UBOOTIMG=out/u-boot-2017.07-${BOARD}.img
+cp -a ${UBOOTBIN} ${UBOOTIMG}
+rm -f ${UBOOTIMG}.gz
+gzip $UBOOTIMG
--- u-boot-sunxi/include/configs/sunxi-common.h
+++ u-boot-sunxi/include/configs/sunxi-common.h
@@ -16,6 +16,12 @@
 #include <asm/arch/cpu.h>
 #include <linux/stringify.h>
 
+#ifdef BPI
+#else
+#undef CONFIG_SYS_PROMPT
+#define	CONFIG_SYS_PROMPT "IoT> "
+#endif
+
 #ifdef CONFIG_OLD_SUNXI_KERNEL_COMPAT
 /*
  * The U-Boot workarounds bugs in the outdated buggy sunxi-3.4 kernels at the
@@ -284,6 +290,10 @@ extern int soft_i2c_gpio_scl;
 #define CONFIG_VIDEO_SUNXI
 
 #define CONFIG_VIDEO_LOGO
+#ifdef BPI
+#else
+#define CONFIG_VIDEO_BMP_LOGO
+#endif
 #define CONFIG_VIDEO_STD_TIMINGS
 #define CONFIG_I2C_EDID
 #define VIDEO_LINE_LEN (pGD->plnSizeX)
@@ -452,6 +462,7 @@ extern int soft_i2c_gpio_scl;
 	func(DHCP, dhcp, na)
 
 #ifdef CONFIG_OLD_SUNXI_KERNEL_COMPAT
+/*
 #define BOOTCMD_SUNXI_COMPAT \
 	"bootcmd_sunxi_compat=" \
 		"setenv root /dev/mmcblk0p3 rootwait; " \
@@ -462,6 +473,19 @@ extern int soft_i2c_gpio_scl;
 		"setenv bootargs console=${console} root=${root} ${extraargs}; " \
 		"ext2load mmc 0 0x43000000 script.bin && " \
 		"ext2load mmc 0 0x48000000 uImage && " \
+		"bootm 0x48000000\0"
+*/
+/* BPI */
+#define BOOTCMD_SUNXI_COMPAT \
+	"bootcmd_sunxi_compat=" \
+		"setenv root /dev/mmcblk0p2 rootwait; " \
+		"if fatload mmc 0 0x44000000 ${bpi}/${board}/${service}/uEnv.txt; then " \
+			"echo Loaded environment from uEnv.txt; " \
+			"env import -t 0x44000000 ${filesize}; " \
+		"fi; " \
+		"setenv bootargs console=${console} root=${root} ${extraargs}; " \
+		"fatload mmc 0 0x43000000 script.bin && " \
+		"fatload mmc 0 0x48000000 uImage && " \
 		"bootm 0x48000000\0"
 #else
 #define BOOTCMD_SUNXI_COMPAT
